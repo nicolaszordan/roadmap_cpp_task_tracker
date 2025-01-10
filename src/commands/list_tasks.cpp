5e665a2e6a86b028aa7558ce50362bcbefc67064
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2025-01-10 18:34:15 +0100

    feat(cmds): :speech_balloon: add `Task` formatter implementation
    add `Task` std::formatter and invoke it where necessary (ie. `ListTask` command)

--- src/commands/list_tasks.cpp
+++ src/commands/list_tasks.cpp
@@ -15,24 +15,29 @@ auto ListTasks::run(const std::vector<std::string>& args) -> std::expected<void,
 {
     if (args.size() == 1) {
         for (const auto& task: task_storage.view_all_tasks()) {
-            std::println("id:{} - description:{}", task.id, task.description);
+            std::println("{}", task);
         }
+
     } else if (args.size() == 2) {
         if (args[1] == "done") {
             for (const auto& task: task_storage.view_all_tasks_done()) {
-                std::println("id:{} - description:{}", task.id, task.description);
+                std::println("{}", task);
             }
+
         } else if (args[1] == "todo") {
             for (const auto& task: task_storage.view_all_tasks_todo()) {
-                std::println("id:{} - description:{}", task.id, task.description);
+                std::println("{}", task);
             }
+
         } else if (args[1] == "in-progress") {
             for (const auto& task: task_storage.view_all_tasks_in_progress()) {
-                std::println("id:{} - description:{}", task.id, task.description);
+                std::println("{}", task);
             }
+
         } else {
             return std::unexpected(CommandError::InvalidArguments);
         }
+
     } else {
         return std::unexpected(CommandError::InvalidArguments);
     }
--- src/task.hpp
+++ src/task.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <optional>
 #include <nlohmann/json.hpp>
+#include <format>
 
 using TaskID = std::uint64_t;
 
@@ -23,8 +24,6 @@ struct Task {
     // TODO:        updated_at
 };
 
-auto operator==(const Task& lhs, const Task& rhs) -> bool;
-
 // use this struct to update a task
 struct TaskUpdate {
     std::optional<std::string>  description = {};
@@ -34,5 +33,49 @@ struct TaskUpdate {
 auto to_json(json& j, const TaskStatus& status) -> void;
 auto from_json(const json& j, TaskStatus& status) -> void;
 
+template<>
+struct std::formatter<TaskStatus, char>
+{
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext&& ctx)
+    {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(const TaskStatus& task_status, FormatContext& ctx) const
+    {
+        switch (task_status) {
+        case TaskStatus::Todo:
+            return std::format_to(ctx.out(), "Todo");
+        case TaskStatus::Done:
+            return std::format_to(ctx.out(), "Done");
+        case TaskStatus::InProgress:
+            return std::format_to(ctx.out(), "InProgress");
+        default:
+            return std::format_to(ctx.out(), "Unknown");
+        }
+    }
+};
+
+auto operator==(const Task& lhs, const Task& rhs) -> bool;
+
 auto to_json(json& j, const Task& task) -> void;
 auto from_json(const json& j, Task& task) -> void;
+
+template<>
+struct std::formatter<Task, char>
+{
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext&& ctx)
+    {
+        return ctx.begin();
+    }
+
+    template<typename FormatContext>
+    auto format(const Task& task, FormatContext& ctx) const
+    {
+        return std::format_to(ctx.out(), "Task(id:{}, status:{}, description:{})", task.id, task.status, task.description);
+    }
+};
+
